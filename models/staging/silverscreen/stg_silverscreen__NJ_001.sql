1002200db4be8c372ff92acd14eb6e0210fab3ad
Mariaan Coltan <cm.rebecca@yahoo.com>
2026-01-07 15:20:45 +0000

    Defined sources and created 5 staging models

---
+++ models/staging/silverscreen/stg_silverscreen__NJ_001.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('silverscreen', 'NJ_001') }}
+
+),
+
+renamed as (
+
+    select
+        DATE_TRUNC(month, timestamp) :: DATE AS transaction_month,
+        movie_id,
+        ticket_amount,
+        transaction_total AS revenue,
+        'NJ_001' AS location_id
+
+    from source
+
+)
+
+select * from renamed
